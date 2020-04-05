d20c4d16befbca486175a943392ec2205de05ab8
Yevhen Chmykhun <evgenii.chmyhun@gmail.com>
2020-04-05 23:03:08 +0200

    Add explanation Single Number 136

--- src/main/java/challenges/april2020/SingleNumber.java
+++ src/main/java/challenges/april2020/SingleNumber.java
@@ -17,6 +17,27 @@ public class SingleNumber {
 }
 
 class Solution {
+
+    /**
+     * Look at it like math, rather than code; using ^ to mean bit-level xor, we can say that
+     * <p>
+     * xor is commutative: A ^ B = B ^ A
+     * xor is associative: (A ^ B) ^ C = A ^ (B ^ C)
+     * xoring with zero does nothing: A ^ 0 = A
+     * xoring something twice removes it: A ^ A = 0
+     * The first two properties imply that any reordering of a sequence of xors will yield the exact same result.
+     * <p>
+     * Therefore, given a sequence that contains duplicates, they will all be removed via xor:
+     * <p>
+     * A ^ B ^ X ^ A ^ B  =  A^A ^ B^B ^ X  =  0 ^ 0 ^ X   =  X
+     * Note that this trick only works if there is a single element that is not duplicate.
+     * If there were several, the result would be the XOR of all non-evenly-duplicated elements:
+     * <p>
+     * A ^ B ^ C ^ A  =  A^A ^ B ^ C  =  B ^ C
+     *
+     * @param nums an array of integers
+     * @return unique integer
+     */
     public int singleNumber(int[] nums) {
         int i = 0;
         for (int num : nums) {
