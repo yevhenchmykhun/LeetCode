6fa2697fed46bdcd8cfac3b983e25936ef32a722
Yevhen Chmykhun <evgenii.chmyhun@gmail.com>
2020-04-05 23:01:13 +0200

    Improve Single Number 136

--- src/main/java/challenges/april2020/SingleNumber.java
+++ src/main/java/challenges/april2020/SingleNumber.java
@@ -1,43 +1,29 @@
 package challenges.april2020;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class SingleNumber {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        assert 3 == solution.singleNumber(new int[]{3});
-        assert 1 == solution.singleNumber(new int[]{2,2,1});
-        assert 4 == solution.singleNumber(new int[]{4,1,2,1,2});
+
+        int result;
+        result = solution.singleNumber(new int[]{3});
+        System.out.println(result);
+
+        result = solution.singleNumber(new int[]{2, 2, 1});
+        System.out.println(result);
+
+        result = solution.singleNumber(new int[]{4, 1, 2, 1, 2});
+        System.out.println(result);
     }
 }
 
 class Solution {
     public int singleNumber(int[] nums) {
-        Counter counter = new Counter();
+        int i = 0;
         for (int num : nums) {
-            counter.put(num);
+            i ^= num;
         }
 
-        return counter.getSingle();
-    }
-}
-
-class Counter {
-    private Map<Integer, Integer> counter = new HashMap<>();
-
-    public void put(int value) {
-        counter.merge(value, 1, (k, v) -> v + 1);
-    }
-
-    public int getSingle() {
-        for (Integer key : counter.keySet()) {
-            if (counter.get(key) == 1) {
-                return key;
-            }
-        }
-
-        return -1;
+        return i;
     }
 
 }
